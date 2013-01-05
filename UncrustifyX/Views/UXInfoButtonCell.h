ef75c6f91557c78c61ed14234d9466d2b5e45dea
Ryan Maxwell <ryanm@xwell.co.nz>
2013-01-06 12:49:50 +1300

    Subclass ButtonCell for drawing highlight info button.

---
+++ UncrustifyX/Views/UXInfoButtonCell.h
@@ -0,0 +1,16 @@
+//
+//  UXInfoButtonCell.h
+//  UncrustifyX
+//
+//  Created by Ryan Maxwell on 6/01/13.
+//  Copyright (c) 2013 Ryan Maxwell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UXInfoButtonCell : NSButtonCell
+
+@property (strong, nonatomic) NSImage *standardStateImage;
+@property (strong, nonatomic) NSImage *downStateImage;
+
+@end
---
+++ UncrustifyX/Views/UXInfoButtonCell.m
@@ -0,0 +1,40 @@
+//
+//  UXInfoButtonCell.m
+//  UncrustifyX
+//
+//  Created by Ryan Maxwell on 6/01/13.
+//  Copyright (c) 2013 Ryan Maxwell. All rights reserved.
+//
+
+#import "UXInfoButtonCell.h"
+
+@implementation UXInfoButtonCell
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    
+    if (self) {
+        _standardStateImage = [NSImage imageNamed:@"Info.png"];
+        _downStateImage = [NSImage imageNamed:@"Info-Down.png"];
+        
+        /* NSButtonCell Coordinate system flipped */
+        _standardStateImage.flipped = YES;
+        _downStateImage.flipped = YES;
+    }
+    
+    return self;
+}
+
+- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
+    [self.standardStateImage drawInRect:cellFrame fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+}
+
+- (void)highlight:(BOOL)flag withFrame:(NSRect)cellFrame inView:(NSView *)controlView {
+    if (flag){
+        [self.downStateImage drawInRect:cellFrame fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
+    } else {
+        [self drawWithFrame:cellFrame inView:controlView];
+    }
+}
+
+@end
